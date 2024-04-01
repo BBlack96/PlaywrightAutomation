41bee6c02191cd413d87f1de528620041872a754
admin <bogdan7450@gmail.com>
2024-04-01 16:07:22 +0300

    Homework-18 fixes according to comments

--- tests/auth.spec.js
+++ tests/auth.spec.js
@@ -7,14 +7,14 @@ test.describe("Positive auth", () => {
 
     test('Positive: Successful registration', async ({page}) => {
         await page.getByRole('button', {name: 'Sign up'}).click();
-        await page.fill('//input[@id=\'signupName\']', 'Bogdan');
-        await page.fill('//input[@id=\'signupLastName\']', 'Chornyi');
-        await page.fill('//input[@id=\'signupEmail\']', 'aqa-bogdan@example.com');
-        await page.fill('//input[@id=\'signupPassword\']', 'Vestell1200');
-        await page.fill('//input[@id=\'signupRepeatPassword\']', 'Vestell1200');
-        await page.click('//button[normalize-space()=\'Register\']');
+        await page.fill("//input[@id='signupName']", 'Bogdan');
+        await page.fill("//input[@id='signupLastName']", 'Chornyi');
+        await page.fill("//input[@id='signupEmail']", 'aqa-bogdan@example.com');
+        await page.fill("//input[@id='signupPassword']", 'Vestell1200');
+        await page.fill("//input[@id='signupRepeatPassword']", 'Vestell1200');
+        await page.click("//button[normalize-space()='Register']");
         await expect(page.getByRole('heading')).toContainText('Garage');
-        await page.getByRole('link', {name: ' Settings'}).click();
+        await page.locator("//a[@routerlink='settings']").click()
         await page.getByRole('button', {name: 'Remove my account'}).click();
         await page.getByRole('button', {name: 'Remove'}).click();
 
@@ -28,41 +28,101 @@ test.describe("Negative auth", () => {
 
     test('Negative: Registration with a wrong name', async ({page}) => {
         await page.getByRole('button', {name: 'Sign up'}).click();
-        await page.fill('//input[@id=\'signupName\']', 'test 😂');
-        await page.fill('//input[@id=\'signupLastName\']', 'Chornyi');
-        await page.fill('//input[@id=\'signupEmail\']', 'aqa-bogdan@example.com');
-        await page.fill('//input[@id=\'signupPassword\']', 'Vestell1200');
-        await page.fill('//input[@id=\'signupRepeatPassword\']', 'Vestell1200');
-        await expect(page.getByRole('paragraph')).toContainText('Name is invalid');
+        await page.fill("//input[@id='signupName']", 'test 😅');
+        await page.fill("//input[@id='signupLastName']", 'Chornyi');
+        await page.fill("//input[@id='signupEmail']", 'aqa-bogdan@example.com');
+        await page.fill("//input[@id='signupPassword']", 'Vestell1200');
+        await page.fill("//input[@id='signupRepeatPassword']", 'Vestell1200');
+        await page.click("//input[@id='signupPassword']");
+        await expect(page.locator("form")).toContainText('Name is invalid');
     });
 
     test('Negative: Different passwords', async ({page}) => {
         await page.getByRole('button', {name: 'Sign up'}).click();
-        await page.fill('//input[@id=\'signupName\']', 'Bogdan');
-        await page.fill('//input[@id=\'signupLastName\']', 'Chornyi');
-        await page.fill('//input[@id=\'signupEmail\']', 'aqa-bogdan@example.com');
-        await page.fill('//input[@id=\'signupPassword\']', 'Vestell1200');
-        await page.fill('//input[@id=\'signupRepeatPassword\']', 'Vestell1100');
-        await expect(page.getByRole('paragraph')).toContainText('Passwords do not match');
+        await page.fill("//input[@id='signupName']", 'Bogdan');
+        await page.fill("//input[@id='signupLastName']", 'Chornyi');
+        await page.fill("//input[@id='signupEmail']", 'aqa-bogdan@example.com');
+        await page.fill("//input[@id='signupPassword']", 'Vestell1200');
+        await page.fill("//input[@id='signupRepeatPassword']", 'Vestell1100');
+        await page.click("//input[@id='signupPassword']");
+        await expect(page.locator("form")).toContainText('Passwords do not match');
     });
 
     test('Negative: Name length more than 20 characters', async ({page}) => {
         await page.getByRole('button', {name: 'Sign up'}).click();
-        await page.fill('//input[@id=\'signupName\']', 'ThisIsMyLengthTestForNameInput');
-        await page.fill('//input[@id=\'signupLastName\']', 'Chornyi');
-        await page.fill('//input[@id=\'signupEmail\']', 'aqa-bogdan@example.com');
-        await page.fill('//input[@id=\'signupPassword\']', 'Vestell1200');
-        await page.fill('//input[@id=\'signupRepeatPassword\']', 'Vestell1100');
-        await expect(page.locator('form')).toContainText('Name has to be from 2 to 20 characters long');
+        await page.fill("//input[@id='signupName']", 'ThisIsMyNameLengthTest');
+        await page.fill("//input[@id='signupLastName']", 'Chornyi');
+        await page.fill("//input[@id='signupEmail']", 'aqa-bogdan@example.com');
+        await page.fill("//input[@id='signupPassword']", 'Vestell1200');
+        await page.fill("//input[@id='signupRepeatPassword']", 'Vestell1200');
+        await page.click("//input[@id='signupPassword']");
+        await expect(page.locator("form")).toContainText('Name has to be from 2 to 20 characters long');
     });
 
     test('Negative: Last name length more than 20 characters', async ({page}) => {
         await page.getByRole('button', {name: 'Sign up'}).click();
-        await page.fill('//input[@id=\'signupName\']', 'Bogdan');
-        await page.fill('//input[@id=\'signupLastName\']', 'ThisIsMyLengthTestForLastNameInput');
-        await page.fill('//input[@id=\'signupEmail\']', 'aqa-bogdan@example.com');
-        await page.fill('//input[@id=\'signupPassword\']', 'Vestell1200');
-        await page.fill('//input[@id=\'signupRepeatPassword\']', 'Vestell1100');
-        await expect(page.locator('form')).toContainText('Last name has to be from 2 to 20 characters long');
+        await page.fill("//input[@id='signupName']", 'Bogdan');
+        await page.fill("//input[@id='signupLastName']", 'ThisIsMyLastNameLengthTest');
+        await page.fill("//input[@id='signupEmail']", 'aqa-bogdan@example.com');
+        await page.fill("//input[@id='signupPassword']", 'Vestell1200');
+        await page.fill("//input[@id='signupRepeatPassword']", 'Vestell1200');
+        await page.click("//input[@id='signupPassword']");
+        await expect(page.locator("form")).toContainText('Last name has to be from 2 to 20 characters long');
+    });
+
+    test('Negative: Invalid email', async ({page}) => {
+        await page.getByRole('button', {name: 'Sign up'}).click();
+        await page.fill("//input[@id='signupName']", 'Bogdan');
+        await page.fill("//input[@id='signupLastName']", 'ThisIsMyLastNameLengthTest');
+        await page.fill("//input[@id='signupEmail']", 'aqa@.com');
+        await page.fill("//input[@id='signupPassword']", 'Vestell1200');
+        await page.fill("//input[@id='signupRepeatPassword']", 'Vestell1200');
+        await page.click("//input[@id='signupPassword']");
+        await expect(page.locator('form')).toContainText('Email is incorrect');
+    });
+
+    test('Negative: Empty Last name', async ({page}) => {
+        await page.getByRole('button', {name: 'Sign up'}).click();
+        await page.fill("//input[@id='signupName']", 'Bogdan');
+        await page.fill("//input[@id='signupLastName']", '');
+        await page.fill("//input[@id='signupEmail']", 'aqa@.com');
+        await page.fill("//input[@id='signupPassword']", 'Vestell1200');
+        await page.fill("//input[@id='signupRepeatPassword']", 'Vestell1200');
+        await page.click("//input[@id='signupPassword']");
+        await expect(page.locator('form')).toContainText('Last name required');
+    });
+
+    test('Negative: Invalid password', async ({page}) => {
+        await page.getByRole('button', {name: 'Sign up'}).click();
+        await page.fill("//input[@id='signupName']", 'Bogdan');
+        await page.fill("//input[@id='signupLastName']", 'Chornyi');
+        await page.fill("//input[@id='signupEmail']", 'aqa-bogdan@example.com');
+        await page.fill("//input[@id='signupPassword']", 'qweqwe');
+        await page.fill("//input[@id='signupRepeatPassword']", 'qweqwe');
+        await page.click("//input[@id='signupPassword']");
+        await expect(page.locator('form')).toContainText('Password has to be from 8 to 15 characters ' +
+            'long and contain at least one integer, one capital, and one small letter');
+    });
+
+    test('Negative: Empty password', async ({page}) => {
+        await page.getByRole('button', {name: 'Sign up'}).click();
+        await page.fill("//input[@id='signupName']", 'Bogdan');
+        await page.fill("//input[@id='signupLastName']", 'Chornyi');
+        await page.fill("//input[@id='signupEmail']", 'aqa-bogdan@example.com');
+        await page.fill("//input[@id='signupPassword']", '');
+        await page.fill("//input[@id='signupRepeatPassword']", '');
+        await page.click("//input[@id='signupPassword']");
+        await expect(page.locator('form')).toContainText('Password required');
+    });
+
+    test('Negative: Empty re-enter password', async ({page}) => {
+        await page.getByRole('button', {name: 'Sign up'}).click();
+        await page.fill("//input[@id='signupName']", 'Bogdan');
+        await page.fill("//input[@id='signupLastName']", 'Chornyi');
+        await page.fill("//input[@id='signupEmail']", 'aqa-bogdan@example.com');
+        await page.fill("//input[@id='signupPassword']", '');
+        await page.fill("//input[@id='signupRepeatPassword']", '');
+        await page.click("//input[@id='signupPassword']");
+        await expect(page.locator('form')).toContainText('Re-enter password required');
     });
 })
